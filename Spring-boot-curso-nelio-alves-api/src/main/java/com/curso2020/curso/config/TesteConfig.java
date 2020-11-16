a79d5954e3cd81e8b084328ce1f2120c175e3dbe
Rubens Leme <rubenss.leme@gmail.com>
2020-11-16 10:51:24 -0300

    Ajuste

--- Spring-boot-curso-nelio-alves-api/src/main/java/com/curso2020/curso/config/TesteConfig.java
+++ Spring-boot-curso-nelio-alves-api/src/main/java/com/curso2020/curso/config/TesteConfig.java
@@ -63,7 +63,9 @@ public class TesteConfig implements CommandLineRunner {
 
 		Usuario u1 = new Usuario(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		Usuario u2 = new Usuario(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
-		usuarioRepositorio.saveAll(Arrays.asList(u1, u2));
+		Usuario u3 = new Usuario(null, "Arthur Leme", "arthur@gmail.com", "99999999", "654321");
+		
+		usuarioRepositorio.saveAll(Arrays.asList(u1, u2, u3));
 
 		Pedido pe1 = new Pedido(null, Instant.parse("2019-06-20T19:53:07Z"), PedidoStatus.PAGO, u1);
 		Pedido pe2 = new Pedido(null, Instant.parse("2019-07-21T03:42:10Z"), PedidoStatus.AGUARDANDO_PAGAMENTO, u2);
